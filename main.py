4fc9f3b4b9b45bc4098f22103750d5cb9ca01e5f
Metin Seylan <metinseylan@192.168.1.15>
2023-01-20 19:16:07 +0300

    added checkpoints callback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-from tensorflow.keras.optimizers.legacy import Adam
 import math
 import preprocess
 
@@ -8,7 +7,7 @@ def calculate_spe(y):
     return int(math.ceil((1. * y) / batch_size))
 
 
-batch_size = 128
+batch_size = 64
 chunk_size = 960000
 data_len = 8746216
 validata_len = 8746216
@@ -25,7 +24,6 @@ train_gen = preprocess.get_generator(data, tokenizer, chunk_size=chunk_size, bat
 test_gen = preprocess.get_generator(data, tokenizer, chunk_size=chunk_size, batch_size=batch_size, label_len=label_len,
                                     input_size=input_len)
 
-
 model = tf.keras.Sequential([
     tf.keras.layers.Embedding(input_dim=vocab_size, output_dim=2048, input_length=150),
     tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(512, return_sequences=True)),
@@ -36,18 +34,22 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(26, activation="softmax")
 ])
 
-model.compile(loss='categorical_crossentropy', optimizer=Adam(0.001), metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
+              metrics=['accuracy'])
 
 steps_per_epoch = calculate_spe(data_len)
 validation_steps = calculate_spe(validata_len)
 
-callback = tf.keras.callbacks.EarlyStopping(patience=3)
+checkpoint_path = "./checkpoints/save_at_{epoch}"
 
-model.fit(train_gen, epochs=10, steps_per_epoch=steps_per_epoch, validation_data=test_gen,
+model.fit(train_gen, epochs=1, steps_per_epoch=steps_per_epoch, validation_data=test_gen,
           validation_batch_size=batch_size,
           validation_steps=validation_steps,
-          callbacks=[callback])
-
-model.evaluate(test_gen)
+          callbacks=[
+              tf.keras.callbacks.ModelCheckpoint(checkpoint_path),
+              tf.keras.callbacks.EarlyStopping(patience=3)
+          ])
 
 model.save('./trained')
+
+model.evaluate(test_gen)
